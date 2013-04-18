d878ad7cb571ac66030ac3ff290623ae07562cab
Nicolas DELSAUT <nicolas.delsaut@gmail.com>
2013-04-18 22:24:24 +0200

    THE commit

--- android/InToads/src/lu/intech/intoads/activity/ControlActivity.java
+++ android/InToads/src/lu/intech/intoads/activity/ControlActivity.java
@@ -1,38 +1,148 @@
 package lu.intech.intoads.activity;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import lu.intech.intoads.R;
-import lu.intech.intoads.R.layout;
-import lu.intech.intoads.R.menu;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import lu.intech.intoads.R;
+import lu.intech.intoads.geolocalisation.GPSTracker;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class ControlActivity extends Activity {
+
+
+    
+    Button btnStart;
+    
+    // GPSTracker class
+    GPSTracker gps;
+  
+    
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_control);
+		setContentView(R.layout.activity_control);		
+		
+		//ToDo : GetTeams & Save
+		GetTeamsAsync gta = new GetTeamsAsync(this);
+		gta.execute();
+		
+		Spinner spinner = (Spinner) findViewById(R.id.team_spinner);
+		
+		btnStart = (Button) findViewById(R.id.button_start);
+		 
+        // show location button click event
+        btnStart.setOnClickListener(new View.OnClickListener() {
+ 
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(ControlActivity.this);
+ 
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+ 
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+ 
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+ 
+            }
+        });
+		
+		//On select
+		//ToDo : GetRiders
+		//ToDo : SaveRiders
+		
+		//On select
+		//ToDo : Save pref
+		//ToDo : Enable start button
 
-		/*Spinner spinner = (Spinner) findViewById(R.id.team_spinner);
-		// Create an ArrayAdapter using the string array and a default spinner
-		// layout
+		/*Spinner spinner = (Spinner) findViewById(R.id.team_spinnerR);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter
 				.createFromResource(this, R.array.array_team,
 						android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);*/
-	}
+		spinner.setAdapter(adapter);
+		
+		
+		
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});*/
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.control, menu);
 		return true;
+	}
+	
+	public void loadTeams(ArrayList<Team> listTeam){
+		Spinner spinner = (Spinner) findViewById(R.id.team_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter
+				.createFromResource(this, R.array.array_team,
+						android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 
 }
--- android/InToads/src/lu/intech/intoads/db/DBInterface.java
+++ android/InToads/src/lu/intech/intoads/db/DBInterface.java
@@ -1,11 +1,29 @@
 package lu.intech.intoads.db;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.DOMException;
+import org.xml.sax.SAXException;
+
+import android.database.Cursor;
+
 /**
  * DB Interface
  * 
  * @author sebastien
  *
  */
-public interface DBInterface {
-
+public interface DBInterface<Entry> {
+	public Entry getFromId(long i);
+	public Cursor getListCursor();
+	public Entry convertCursorToEntry(Cursor c);
+	public long insert(Entry entry);
+	public int update(Entry entry);
+	public boolean isExist(int id);
+	public int delete(Entry news);
+	public int delete(int id);
+	public void updateTableFromXML() throws DOMException, IOException, ParserConfigurationException, SAXException, MalformedURLException;
 }
