f5c5abda78959f277a4023a10c2a5df37e803657
Pierrick <pierrick.bena@in*tech.lu>
2013-04-18 19:52:49 +0200

    19h52

---
+++ android/InToads/src/lu/intech/intoads/geolocalisation/geolocalisation.java
@@ -0,0 +1,13 @@
+package lu.intech.intoads.geolocalisation;
+
+public class geolocalisation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
