6e407899932eb1e770fac43dcd3a4bc6c8194b48
jldellagiustina <jean-louis.della-giustina@intech.lu>
2013-04-18 20:00:01 +0200

    controlleurs
    Signed-off-by: jldellagiustina <jean-louis.della-giustina@intech.lu>

---
+++ backend/grails-app/controllers/backend/RiderController.groovy
@@ -0,0 +1,12 @@
+package backend
+
+class RiderController {
+
+    /**
+     * retourne le détail d'un coureur
+     * @return
+     */
+    def index() {
+        render("détail coureur")
+    }
+}
