af07058c502ee29a4412a8eb1e7140a2b209cd76
Maximilien Roustan <maximilien.roustan@intech.lu>
2013-04-18 19:17:02 +0200

    Initialisation du Storyboard et de CoreData

---
+++ ios/InToads/TeamViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeamViewController.h
+//  InToads
+//
+//  Created by Max Roustan on 18/04/13.
+//  Copyright (c) 2013 InTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeamViewController : UIViewController
+
+@end
---
+++ ios/InToads/TeamViewController.m
@@ -0,0 +1,38 @@
+//
+//  TeamViewController.m
+//  InToads
+//
+//  Created by Max Roustan on 18/04/13.
+//  Copyright (c) 2013 InTech. All rights reserved.
+//
+
+#import "TeamViewController.h"
+
+@interface TeamViewController ()
+
+@end
+
+@implementation TeamViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
